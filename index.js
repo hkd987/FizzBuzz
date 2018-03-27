d3b259bd3807281ac3f9f9d3d943221955372179
hkd987 <lundinmatthews@me.com>
2018-03-27 10:09:59 -0500

    unit test
    idk why but lets get some fizzbuzz unit test

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // FIZZBUZZ
-let output = ''
 
-for (let i = 1; i < 101; i++) {
-  if (i % 3 === 0) output += 'Fizz'
-  if (i % 5 === 0) output += 'Buzz'
-  if (output === '') output += i
-  console.log(output)
-  output = ''
+const fizzle = () => {
+  const output = []
+  for (let i = 1; i < 101; i++) {
+    let holder = ''
+    if (i % 3 === 0) holder += 'Fizz'
+    if (i % 5 === 0) holder += 'Buzz'
+    if (holder === '') holder += i
+    output.push(holder)
+    holder = ''
+  }
+  return output
 }
+
+console.log(fizzle())
+
+
+module.exports = fizzle

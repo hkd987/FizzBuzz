0019a01cc5f639b2d4a2b6e377e42bf7bcc899fa
hkd987 <lundinmatthews@me.com>
2018-03-27 08:30:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// FIZZBUZZ
+let output = ''
+
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0) output += 'Fizz'
+  if (i % 5 === 0) output += 'Buzz'
+  if (output === '') output += i
+  console.log(output)
+  output = ''
+}
